5e01502256438184631703d318b2b11adc859be0
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-03-05 22:07:45 +0700

    controller

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //public function index()
+   // {
+    //    return "Selamat Datang";
+    //}
+
+   /// public function about()
+   // {
+     //   return "Nama: Ananda <br> NIM: 2341720048";
+    //}
+
+    //public function articles($id)
+    //{
+      //  return "Ini adalah halaman artikel dengan ID: " . $id;
+    //}
+//}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello',function () {
-    return 'Hello World';
-});
+Route::get('/hello', [WelcomeController::class, 'hello']);
+
+Route::get('/', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles{id}', [PageController::class, 'articles']);
+
+
+
 
 Route::get('/world',function () {
     return ' World';
