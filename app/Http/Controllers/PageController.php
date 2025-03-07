dcf727d33a11b9461f4dc6a5b09a5438b19827c4
anandarahmawati24 <rahmawatiananda571@gmail.com>
2025-03-07 10:56:27 +0700

    praktikum 2 controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PageController extends Controller
-{
+//class PageController extends Controller
+//{
     //public function index()
    // {
     //    return "Selamat Datang";
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,10 +26,9 @@ Route::get('/', function () {
 
 Route::get('/hello', [WelcomeController::class, 'hello']);
 
-Route::get('/', [PageController::class, 'index']);
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/articles{id}', [PageController::class, 'articles']);
-
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
 
 
 
@@ -56,3 +60,12 @@ Route::get('/articles/{id}', function ($id) {
 Route::get('/user{name?}', function ($name='John') {
     return 'Nama saya '.$name;
 });
+
+ Route::resource('photos', PhotoController::class);
+
+ Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+    ]);
+    Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+    ]);
